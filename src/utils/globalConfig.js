6213d1dec73822b0951f35ff43ed147029df0c22
chinthakaSenarathna <chironprabath@gmail.com>
2024-07-06 09:12:48 +0530

    set the globalConfig

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -15,6 +15,7 @@ export const MY_LOGS_URL = 'https://localhost:7026/api/Log/mine';
 export const ADD_USER_IMAGE = 'https://localhost:7026/api/UserImage/AddUserImage';
 export const CREATE_OUT_MESSAGES_URL = 'https://localhost:7026/api/OutMessages/create';
 export const GET_OUT_MESSAGES = 'https://localhost:7026/api/OutMessages/get';
+export const UPDATE_USER_PASSWORD = 'https://localhost:7026/api/UserPassword/updateUserPassword';
 
 // Auth Routes
 export const PATH_AFTER_REGISTER = PATH_PUBLIC.login;
