f86a3827dce7e0e799dc704d09c793df01cd02cd
karunarathnawmwgms <122153208+karunarathnawmwgms@users.noreply.github.com>
2024-07-21 15:56:46 +0530

    chh

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -1,7 +1,8 @@
+
 export const PATH_PUBLIC = {
     home: '/',
-    register: '/register',
-    login: '/login',
+    register: '/user/register',
+    login: '/user/login',
     unauthorized: '/unauthorized',
     notFound: '/404',
 }
@@ -22,6 +23,7 @@ export const PATH_DASHBOARD_USER = {
 
 export const PATH_DASHBOARD_ADMIN = {
     home: '/admin',
+    login: '/admin/login',
     dashboard: '/admin/dashboard',
     dashboardAdmin: '/admin/dashboard/admin-dashboard',
     usersManagement: '/admin/dashboard/users-management',
