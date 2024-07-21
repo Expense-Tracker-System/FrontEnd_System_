cb9340df9076c752743b1879996ee3c75a1c738b
Spcoderr <sajithpriyashan105@gmail.com>
2024-07-22 00:43:12 +0530

    Changes Made

--- src/components/OrganizationComponenets/CreateOrganizationPage.jsx
+++ src/components/OrganizationComponenets/CreateOrganizationPage.jsx
@@ -1,27 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-  Box, Button, TextField, Typography, List, ListItem, ListItemText,
-  ListItemSecondaryAction, IconButton, CircularProgress, useMediaQuery, useTheme
-} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import SearchIcon from '@mui/icons-material/Search';
-import CheckIcon from '@mui/icons-material/Check';
-import axiosInstance from '../../utils/axiosInstance';
-import { USERS_LIST_URL, CREATE_ORGANIZATION } from '../../utils/globalConfig';
-import { toast } from 'react-hot-toast'; // Assuming you use react-hot-toast for toasts
-import useAuth from '../../hooks/useAuth.hook';
+  Box,
+  Button,
+  TextField,
+  Typography,
+  CircularProgress,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import axiosInstance from "../../utils/axiosInstance";
+import { USERS_LIST_URL, CREATE_ORGANIZATION } from "../../utils/globalConfig";
+import { toast } from "react-hot-toast";
+import useAuth from "../../hooks/useAuth.hook";
+import Autocomplete from "@mui/lab/Autocomplete";
 
 const CreateOrganizationPage = () => {
-  const [title, setTitle] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
+  const [title, setTitle] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const [users, setUsers] = useState([]);
   const [selectedUsers, setSelectedUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [showUserList, setShowUserList] = useState(true);
   const [submitAttempted, setSubmitAttempted] = useState(false);
   const navigate = useNavigate();
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   const { user } = useAuth();
 
   useEffect(() => {
@@ -31,18 +35,16 @@ const CreateOrganizationPage = () => {
         const response = await axiosInstance.get(USERS_LIST_URL);
         setUsers(response.data);
       } catch (error) {
-        toast.error('An Error occurred. Please contact admin.');
+        toast.error("An Error occurred. Please contact admin.");
       } finally {
         setLoading(false);
       }
     };
     fetchUsers();
-  }, [searchTerm]);
+  }, []);
 
-  const handleSelectUser = (user) => {
-    if (!selectedUsers.find(u => u.id === user.id)) {
-      setSelectedUsers((prevSelected) => [...prevSelected, user]);
-    }
+  const handleSelectUser = (event, newValue) => {
+    setSelectedUsers(newValue);
   };
 
   const handleCreateOrganization = async () => {
@@ -50,90 +52,95 @@ const CreateOrganizationPage = () => {
     if (!title.trim()) {
       return;
     }
-    const organizationData = { title, users: selectedUsers };
+
+    // Prepare the data for the API request
+    const organizationData = {
+      title,
+      users: selectedUsers.map((user) => ({
+        id: user.id,
+        userName: user.userName,
+      })),
+    };
+
     try {
       setLoading(true);
-      console.log(selectedUsers);
-      const response = await axiosInstance.post(CREATE_ORGANIZATION, organizationData);
+      const response = await axiosInstance.post(
+        CREATE_ORGANIZATION,
+        organizationData
+      );
       setLoading(false);
       setSelectedUsers([]);
-      setTitle('');
-      setShowUserList(false);
+      setTitle("");
       setSubmitAttempted(false);
-      toast.success('Organization created successfully');
+      toast.success("Organization created successfully");
       // navigate('/view-organization');
     } catch (error) {
       setLoading(false);
-      toast.error('An Error occurred. Please contact admin.');
+      toast.error("An Error occurred. Please contact admin.");
     }
   };
 
+  const filteredUsers = users.filter((user) =>
+    user.userName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <Box sx={{ padding: '20px' }}>
-      <Typography variant="h4" gutterBottom>Create Organization</Typography>
+    <Box sx={{ padding: "20px" }}>
+      <Typography variant="h4" gutterBottom>
+        Create Organization
+      </Typography>
       <TextField
         label="Organization Title"
         variant="outlined"
         fullWidth
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-        sx={{ marginBottom: '20px' }}
+        sx={{ marginBottom: "20px" }}
         error={submitAttempted && !title.trim()}
-        helperText={submitAttempted && !title.trim() ? "Organization title is required." : ''}
+        helperText={
+          submitAttempted && !title.trim()
+            ? "Organization title is required."
+            : ""
+        }
       />
 
-      <TextField
-        label="Search Users"
-        variant="outlined"
-        fullWidth
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        sx={{ marginBottom: '20px', }}
-        InputProps={{
-          endAdornment: (
-          
-              <SearchIcon style={{}} />
-            
-          )
+      <Autocomplete
+        multiple
+        options={filteredUsers}
+        getOptionLabel={(option) => option.userName}
+        onInputChange={(event, newInputValue) => {
+          setSearchTerm(newInputValue);
         }}
+        onChange={handleSelectUser}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search Users"
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: "20px" }}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: <SearchIcon />,
+            }}
+          />
+        )}
       />
 
-      {showUserList && (
-        <>
-          {loading ? <CircularProgress /> : (
-            <List>
-              {users.filter(user_ => user_.userName != user.userName).map((user_) => (
-                <ListItem key={user_.id}>
-                  <ListItemText primary={user_.userName} />
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" onClick={() => handleSelectUser(user_)}>
-                      
-                      <CheckIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
-              ))}
-            </List>
-          )}
-
-<Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', marginTop: '20px' }}>
-  <Button 
-    variant="contained" 
-    color="success" 
-    onClick={handleCreateOrganization} 
-    sx={{ 
-      bgcolor: 'black', 
-      width: '500px', 
-      height: '40px'
-    }}
-  >
-    Create
-  </Button>
-</Box>
-        </>
-      )}
+      <div style={{ display:'flex',justifyContent:'center'}}>
+      <button
+        className="view-button"
+        onClick={handleCreateOrganization}
+        disabled={loading}
+        style={{width:'500px'}}
+      >
+        {loading ? "Loading..." : "Create Organization"}
+      </button>
+      </div>
+     
     </Box>
   );
 };
 
 export default CreateOrganizationPage;
+
